9f9d02842e87de2168b3aa6cc3da3c6e1613ffd8
Sophie Engobor <sophietengobor@gmail.com>
2022-03-22 17:58:53 -0500

    almost done

--- src/pages/FilmsDetails.jsx
+++ src/pages/FilmsDetails.jsx
@@ -3,59 +3,26 @@ import { useParams } from "react-router-dom";
 
 const FilmsDetails = () => {
   const { filmsid } = useParams([]);
-  const [films, setfilms] = useState([]);
+  const [film, setfilm] = useState([]);
 
   useEffect(() => {
-    fetch("https://ghibliapi.herokuapp.com/films")
+    fetch(`https://ghibliapi.herokuapp.com/films/${filmsid}`)
       .then((res) => res.json())
-      .then((allfilms) => setfilms(allfilms));
+      .then((film) => setfilm(film));
   }, []);
 
   return (
     <>
-      <main className="morecontainer">
+      <div className="morecontainer">
         <section className="row justify-content-center">
-          {films.map((films) => (
-            <div className="col-md-6" key={`films-card-${films.id}`}>
-              <div className="card col-md-6">
-                <h1 className="text-primary">Hello {filmsid}</h1>
-                <p className="card-subtitle">{films.description}</p>
-              </div>
+          {film.map((film) => (
+            <div className="col-md-6" key={film.id}>
+                <h1 className="text-primary">Hello</h1>
+                <p className="card-subtitle">{film.description}</p>
             </div>
           ))}
         </section>
-      </main>
-      {/* <div class="container">
-        <section className="row justify-content-center mt-5">
-          {films.map((films) => (
-            <div className="col-md-6" key={`films-card-${films.id}`}>
-              <div className="card shadow my-2">
-                <img src={films.image} class="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h4 className="card-header">{filmsid}</h4>
-                  <p className="card-subtitle">Director: {films.director}</p>
-                  <p className="card-subtitle">Producer: {films.producer}</p>
-                  <p className="card-subtitle">
-                    Released in: {films.release_date}
-                  </p>
-                  <p className="card-subtitle">
-                    Rotten Tomatoes Score: {films.rt_score}
-                  </p>
-                  <div className="card-footer">
-                    <p className="card-subtitle">{films.description}</p>
-                    <a
-                      href="https://ghibliapi.herokuapp.com/films"
-                      class="card-link"
-                    >
-                      API Resource Link
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </section>
-      </div> */}
+      </div>
     </>
   );
 };
